c2e1352bd2dd360e580d29a1c9bf78a48a378c7a
Goof Hendriks <goofhendriks@hotmail.com>
2024-02-29 12:39:11 +0100

    merged the two movie files, filtered by year 2000-2023, final file name is movies

--- imdbexploration.R
+++ imdbexploration.R
@@ -2,16 +2,22 @@ library(tidyverse)
 
 setwd("C:/Users/apper/OneDrive/MAy5b1/SkillsDaPr")
 
-title_basics_data <- read_tsv('title.basics.tsv.gz', n_max = 100)
+title_basics_data <- read_tsv('title.basics.tsv.gz')
 
-title_ratings_data <- read_tsv('title.ratings.tsv.gz', n_max = 100)
+title_ratings_data <- read_tsv('title.ratings.tsv.gz')
 
-view(title_basics_data)
-view(title_ratings_data)
+head(title_basics_data)
+head(title_ratings_data)
+table(title_basics_data$startYear)
 
+#filter for years
+filtered_title_basics <- title_basics_data %>% filter(startYear >= 2000 & startYear <= 2023)
 
 
 # merge the two files
-merged_lists <- merge(title_basics_data, title_ratings_data, by = "tconst", all = TRUE )
+merged_lists <- filtered_title_basics %>% left_join(title_ratings_data, by= 'tconst')
 
-#filter for years
+#remove NA from the ratings collumn
+movies <- merged_lists %>% na.omit(merged_lists$averageRating)
+
+#final list is called movies for further data wranglin
