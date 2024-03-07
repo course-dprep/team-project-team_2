55dd40d233a7304cee66ff0b01c6b7604ae299c0
GoofHendriks <goofhendriks@hotmail.com>
2024-03-07 16:14:27 +0100

    cleaned up the Download_data.R file, it now only downloads the URLS that we will use for our project

--- src/data-preparation/Download_data.R
+++ src/data-preparation/Download_data.R
@@ -1,25 +1,23 @@
-install.packages("tidyverse", repos='http://cran.us.r-project.org')
-install.packages("dplyr", repos='http://cran.us.r-project.org')
-install.packages("readr", repos='http://cran.us.r-project.org')
+#install packages
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("readr")
+install.packages("tidyr")
+install.packages("ggplot2")
 library(tidyverse)
 library(dplyr)
-
-dir.create("../../data")
+library(ggplot2)
+library(tidyr)
 
 ## Get the data from the URLs
 options(timeout = max(1000, getOption("timeout")))
 urls <- c(
-  'https://datasets.imdbws.com/name.basics.tsv.gz',
-  'https://datasets.imdbws.com/title.akas.tsv.gz',
   'https://datasets.imdbws.com/title.basics.tsv.gz',
-  'https://datasets.imdbws.com/title.crew.tsv.gz',
-  'https://datasets.imdbws.com/title.episode.tsv.gz',
-  'https://datasets.imdbws.com/title.principals.tsv.gz',
   'https://datasets.imdbws.com/title.ratings.tsv.gz'
 )
 
 ## Namin the URL's
-names <- c("Names", "Akas", "Crew", "Episode", "Principals", "Ratings")
+names <- c("Basics", "Ratings")
 
 for (i in 1:length(urls)) {
   download.file(urls[i], paste0("../../data/", names[i], ".tsv.gz"), mode = "wb")
