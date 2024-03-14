aa381cd2bd209cdf0eae5ba0a7fbec1f853490fa
Robin Hogenboom <r.hogenboom@tilburguniversity.edu>
2024-03-14 10:48:06 +0100

    datacode.R

--- src/data-preparation/imdbexploration.R
+++ src/data-preparation/imdbexploration.R
@@ -1,17 +1,28 @@
 # Setup
 library(tidyverse)
+library(dplyr)
 
-#setwd("C:/dPrep/data/title.basics.tsv")
-
+#Reading relevant datasets
 title_basics_data <- read_tsv('title.basics.tsv.gz')
-
 title_ratings_data <- read_tsv('title.ratings.tsv.gz')
 
+#Filtering dataset
+data.frame(title_basics_data)
+#Filtering for year
+filtered_title_basics <- title_basics_data[title_basics_data$startYear >= 2000 & title_basics_data$startYear <= 2023,]
 
-# merge the two files
-merged_lists <- filtered_title_basics %>% left_join(title_ratings_data, by= 'tconst')
+#Filtering for movies
+filtered_title_basics <- filtered_title_basics[filtered_title_basics$titleType == 'movie',]
+
+#Filtering for number of votes >250
+filtered_title_ratings_data <- title_ratings_data[title_ratings_data$numVotes > 1000,]
+
+#merge the two files
+merged_lists <- filtered_title_basics %>% left_join(filtered_title_ratings_data, by= 'tconst')
 
 #remove NA from the ratings collumn
 movies <- merged_lists %>% na.omit(merged_lists$averageRating)
 
-#final list is called movies for further data wranglin
+#final list is called movies for further data wrangling
+
+
